8886da607baedc0a36400d11c391d2cb8fd0715d
Facundo López Llopis <faculopllop@gmail.com>
2022-05-20 17:59:57 -0300

    corrección

--- 1-PB/Clase_4/divisores.ts
+++ 1-PB/Clase_4/divisores.ts
@@ -1,9 +1,13 @@
 import * as rl from 'readline-sync';
 
+function esMultiplo(numero1:number, numero2:number): boolean {
+    return numero2%numero1==0
+}
+
 function cantidadDivisores(numero:number): number {
     let suma: number = 0;
     for (let i = 1; i <= numero; i++) {
-        if(numero%i==0) {
+        if(esMultiplo(i, numero)) {
             suma++
         }
     }
