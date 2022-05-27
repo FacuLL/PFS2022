c8a7c614e46da62246e8fd2623401ba8738c4494
Facundo López Llopis <faculopllop@gmail.com>
2022-05-27 18:35:01 -0300

    Eliminacion de clase 5 en clase 6

--- 1-PB/Clase_5/arreglo3.ts
+++
@@ -1,9 +0,0 @@
-import * as rl from 'readline-sync';
-
-let cantidadNombres: number = rl.questionInt('Ingrese la cantidad de nombres: ');
-let nombres: number[] = new Array(cantidadNombres);
-for (let i = 0; i < cantidadNombres; i++) {
-    nombres[i] = rl.question(`Inserte el nombre de la persona ${i+1}: `);
-    console.log(`${i+1}. ${nombres[i]}`);
-}
-    
