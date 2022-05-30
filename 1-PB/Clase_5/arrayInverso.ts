9433cd638ebcabad378bbb6a6a50a18b86725575
Facundo López Llopis <faculopllop@gmail.com>
2022-05-30 19:09:55 -0300

    correccion

--- 1-PB/Clase_5/arrayInverso.ts
+++ 1-PB/Clase_5/arrayInverso.ts
@@ -1,7 +1,8 @@
 import * as rl from 'readline-sync';
 let cantNumeros: number = rl.questionInt("Ingrese la cantidad de numeros: ");
-let numeros: number[] = new Array(cantNumeros);
+let numerosInverso: number[] = new Array(cantNumeros);
 for (let index = 0; index < cantNumeros; index++) {
-    numeros[index] = rl.questionInt(`Ingrese el numero ${index+1}: `);
+    numerosInverso[cantNumeros-index-1] = rl.questionInt(`Ingrese el numero ${index+1}: `);
+
 }
-console.log(`La salida es ${numeros.reverse().join(' ')}`);
+console.log(`La salida es ${numerosInverso.join(' ')}`);
